47bbefee395d52c5cb522c499c4aedbe1de9c7de
Yannik Wailzer <yannik.wailzer@htl.rennweg.at>
2022-01-26 17:16:57 +0100

    Add chip select

--- src/components/TrackComponent.vue
+++ src/components/TrackComponent.vue
@@ -1,8 +1,15 @@
 <template>
   <ion-text>
-    <ion-input placeholder="Enter Tag"/>
+    <ion-input placeholder="Enter Tag..." @input="event => tags = event.target.value" />
+    <div v-show="tags">
+      <ion-chip v-for="tag in tagsList" :key="tag">
+        <ion-label>{{ tag }}</ion-label>
+      </ion-chip>
+    </div>
     <ion-button fill="clear" id="begin">{{ beginDate ? formatDateTime(beginDate) : 'Startzeit' }}</ion-button>
     <ion-button fill="clear" id="end">{{ endDate ? formatDateTime(endDate) : 'Endzeit' }}</ion-button>
+    <ion-input></ion-input>
+
     <ion-button>Add Time</ion-button>
 
     <date-picker trigger="begin" @input="value => beginDate = value" />
@@ -19,14 +26,18 @@ export default {
   name: "TrackComponent",
   components: {DatePicker, IonButton },
   data: () => ({
-    tagInput: null,
-    modalBegin: false,
-    modalEnd: false,
+    tags: '',
     beginDate: null,
     endDate: null,
   }),
   methods: {
     formatDateTime,
+  },
+  computed: {
+    tagsList() {
+      console.log(this.tags)
+      return [...new Set(this.tags.trim().split(/\s+/g))];
+    }
   }
 }
 </script>
