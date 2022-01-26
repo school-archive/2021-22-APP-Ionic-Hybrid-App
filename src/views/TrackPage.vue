5d4312d138e685037aa1400fbc2813198ad8bea6
Nils <sensentod@gmail.com>
2022-01-26 17:17:21 +0100

    added filtered entries variable

--- src/views/TrackPage.vue
+++ src/views/TrackPage.vue
@@ -2,7 +2,7 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Der Time Treker</ion-title>
+        <ion-title>Der Time Trekker</ion-title>
       </ion-toolbar>
     </ion-header>
 
@@ -10,12 +10,22 @@
       <track-component/>
 <br>
       <ion-button @click="addTestEntry">Add test entry</ion-button>
-      <ion-button @click="filterModalOpen = true" color="dark" fill="clear">Filter&nbsp;<ion-badge color="dark">inactive</ion-badge></ion-button>
+      <ion-button @click="filterModalOpen = true" color="dark" fill="clear">Filter&nbsp;<ion-badge :color="filtersAreActive ? 'dark' : 'light'">{{filtersAreActive ? '' : 'in'}}active</ion-badge></ion-button>
+
+      <ion-modal
+          :is-open="filterModalOpen"
+          :swipe-to-close="true"
+          :presenting-element="$parent.$refs.ionRouterOutlet"
+          @didDismiss="filterModalOpen = false"
+      >
+<!--        <Modal :data="data"></Modal>-->
+      </ion-modal>
 
       <ion-list>
         <ion-list-header>Tracked Time Entries</ion-list-header>
-        <time-entry v-for="(timeEntry, i) in this.entries" :key="timeEntry.tags + timeEntry.startTime + timeEntry.endTime"
-                    @delete="deleteEntry(i)"
+        <time-entry v-for="timeEntry in (filtersAreActive ? filteredEntries : entries)"
+                    :key="timeEntry.tags + timeEntry.startTime + timeEntry.endTime"
+                    @delete="deleteEntry(entries.indexOf(timeEntry))"
                     :tags="timeEntry.tags" :from="timeEntry.startTime" :until="timeEntry.endTime"/>
       </ion-list>
     </ion-content>
@@ -34,6 +44,10 @@ export default {
   data: () => ({
     store: null,
     entries: [],
+    activeFilters: {
+      tags: [],
+      date: null,
+    },
     filterModalOpen: false,
   }),
   async mounted() {
@@ -78,6 +92,21 @@ export default {
       let entries = JSON.parse(await this.store.get("data"));
       console.log(entries)
       if (entries !== null) this.entries = entries;
+    }
+  },
+  computed: {
+    filtersAreActive() {
+      return (this.activeFilters.tags !== null && this.activeFilters.tags.length > 0) || this.activeFilters.date !== null;
+    },
+    filteredEntries() {
+      return this.entries
+          .filter(entry =>
+            (this.activeFilters.tags === null || this.activeFilters.tags.length === 0 || ! this.activeFilters.tags.some(tag => ! entry.tags.contains(tag))) &&
+              (this.activeFilters.date === null ||
+                  this.activeFilters.date % (24 * 60 * 60 * 1000) >= entry.startDate % (24 * 60 * 60 * 1000) &&
+                  this.activeFilters.date % (24 * 60 * 60 * 1000) <= entry.endDate % (24 * 60 * 60 * 1000)
+              )
+          );
     }
   }
 }
